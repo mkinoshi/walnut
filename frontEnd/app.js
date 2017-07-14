43556739b07d5bf069d040346f13b5264029d12e
omeshomes <omid.rooholfada@yale.edu>
2017-07-14 09:07:01 -0700

    just testing

--- frontEnd/app.js
+++ frontEnd/app.js
@@ -1,5 +1,5 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(<p>React lives!</p>,
    document.getElementById('root'));
