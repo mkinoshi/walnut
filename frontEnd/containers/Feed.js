aaa8e94f6d4f742735c0612ef285f41ed00fb730
mkinoshi <mkinoshi@colby.edu>
2017-07-17 20:47:12 -0700

    added some style

---
+++ frontEnd/containers/Feed.js
@@ -0,0 +1,101 @@
+// maps through posts and renders Post
+// connect
+import React from 'react';
+import { connect } from 'react-redux';
+import Post from '../components/home/feed/Post';
+import PropTypes from 'prop-types';
+import FilterPref from './FilterPref';
+
+
+const styles = {
+  outer: {
+    borderWidth: '1px',
+    width: '24%',
+    height: '100px',
+    borderRadius: '25px',
+    marginTop: '-120px',
+    borderTop: 'solid'
+  }
+};
+
+class Feed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showFilterPref: false,
+    };
+  }
+
+  toggleFilterPref() {
+    this.setState({showFilterPref: !this.state.showFilterPref});
+  }
+
+
+  filterData(data) {
+    const checkedFilterObject = this.props.data.filters.filter((filter) => (filter.checked === true));
+    const getFields = (input, field) => {
+      const output = [];
+      for(let i = 0; i < input.length; ++i) {
+        output.push(input[i][field]);
+      }
+      return output;
+    };
+    // filters array of tags
+    const filters = getFields(checkedFilterObject, 'name');
+    // if the array zero return the entire unfiltered array
+    if(filters.length === 0) {
+      return data;
+    }
+    const final = data.posts.filter((post) => {
+      const findOne = (haystack, arr) => {
+        return arr.some(tag => haystack.indexOf(tag) >= 0);
+      };
+      if(findOne(filters, post.tags) === true) {
+        return post;
+      }
+      return null;
+    });
+    const filteredState = {filters: data.filters, posts: final};
+    return filteredState;
+  }
+
+  render() {
+    const filteredPosts = this.filterData(this.props.data).posts;
+    return (
+      <div>
+        <div className="col-xs-3"><h1>hellojejejejejejejej</h1></div>
+        <div className="col-xs-12">
+          <div className="col-xs-2" style={{}}>
+          <div className="discoverButton" style={{}}>
+            <a style={{backgroundColor: '#FF5657'}}
+              className="waves-effect waves-light btn"
+              onClick={() => (this.toggleFilterPref())}>Discover</a>
+          </div>
+          {this.state.showFilterPref ? <FilterPref filterChange={(name) => (this.filterChange(name))}/> : <p></p>}
+          </div>
+          <div className="col-xs-8">
+            {filteredPosts.map((post) => (
+              <Post postData={post} newLike={() => (this.props.newLike(post.postId))}/>
+            ))}
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+}
+
+Feed.propTypes = {
+  data: PropTypes.object,
+  newLike: PropTypes.func
+};
+
+const mapStateToProps = (state) => ({
+  data: state.discoverReducer
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  newLike: (id) => dispatch({type: 'NEW_LIKE', postId: id})
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Feed);
