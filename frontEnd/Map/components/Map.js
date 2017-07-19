bde86e88e8b79418048a5a735e947d2c680d36d6
omeshomes <omid.rooholfada@yale.edu>
2017-07-18 17:27:53 -0700

    map helper functions

--- frontEnd/Map/components/Map.js
+++ frontEnd/Map/components/Map.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import ReactMapboxGl, { Layer, Feature, Marker, Popup, Cluster } from 'react-mapbox-gl';
+import CircleIcon from 'react-icons/lib/fa/circle';
 // import Iframe from 'react-iframe';
-const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
 const styles = {
   mapContainer: {
@@ -9,34 +10,121 @@ const styles = {
   map: {
     height: '100vh',
     width: '80vw'
+  },
+  image: {
+    height: '50',
+    width: '50'
+  },
+  marker: {
+    color: 'red',
+    fontSize: '30px'
+  },
+  clusterMarker: {
+    color: 'blue',
+    fontSize: '30px'
   }
 };
 
-class Map extends React.Component {
+const Map = ReactMapboxGl({
+  accessToken: 'pk.eyJ1Ijoib21lc2hvbWVzIiwiYSI6ImNqNTh2cXoxZjAxa2QzM3FxaWgxaDEzbzcifQ.rBTIS3ct7ZxUTR1HGW-cXg'
+});
 
-  componentDidMount() {
-    mapboxgl.accessToken = 'pk.eyJ1Ijoib21lc2hvbWVzIiwiYSI6ImNqNTh2cXoxZjAxa2QzM3FxaWgxaDEzbzcifQ.rBTIS3ct7ZxUTR1HGW-cXg';
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
-    });
+
+class MapComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      center: [-103.59179687498357, 40.66995747013945],
+      zoom: [3],
+      clicked: [],
+      users: [
+        {
+          name: 'Eli Badgio',
+          profileURL: 'http://cdnak1.psbin.com/img/mw=160/mh=210/cr=n/d=q864a/dpe4wfzcew4tph99.jpg',
+          location: [-122.4199537, 38.7775032]
+        },
+        {
+          name: 'Eli Badgio',
+          profileURL: 'http://cdnak1.psbin.com/img/mw=160/mh=210/cr=n/d=q864a/dpe4wfzcew4tph99.jpg',
+          location: [-122.4199537, 37.7775032]
+        },
+        {
+          name: 'Eli Badgio',
+          peofileURL: 'http://cdnak1.psbin.com/img/mw=160/mh=210/cr=n/d=q864a/dpe4wfzcew4tph99.jpg',
+          location: [-122.4199537, 36.7775032]
+        },
+        {
+          name: 'Eli Badgio',
+          peofileURL: 'http://cdnak1.psbin.com/img/mw=160/mh=210/cr=n/d=q864a/dpe4wfzcew4tph99.jpg',
+          location: [-122.4199537, 37.8775032]
+        },
+        {
+          name: 'Eli Badgio',
+          peofileURL: 'http://cdnak1.psbin.com/img/mw=160/mh=210/cr=n/d=q864a/dpe4wfzcew4tph99.jpg',
+          location: [-122.4199537, 37.77757]
+        }
+      ]};
+  }
+
+  changeCenter(coordinates) {
+    // coordinates given in array format
+    this.setState({center: coordinates});
+  }
+
+  changeZoom(zoom) {
+    // zoom given as integer
+    this.setState({zoom: [zoom]});
   }
 
   render() {
+    const clusterMarker = (coordinates) => {
+      return (
+        <Marker
+        coordinates={coordinates}
+        style={styles.clusterMarker}
+        >
+          <CircleIcon />
+        </Marker>
+        );
+    };
     return (
-      // <div style={styles.map}>
-      //   <Iframe
-      //     width="400"
-      //     height="400"
-      //     frameBorder="0"
-      //     url="https://www.google.com/maps/embed/v1/place?key=AIzaSyCRsfBv1y4taRhb2nGyRue4a5rEi3KAtiQ
-      //       &q=Space+Needle,Seattle+WA" allowFullScreen />
-      // </div>
-      <div className="mapContainer" style={styles.mapContainer}>
-        <div id="map" style={styles.map}></div>
-      </div>
+      <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        containerStyle={{
+          height: '100vh',
+          width: '100vw'
+        }}
+        center={this.state.center}
+        zoom={this.state.zoom}
+        >
+          <Layer
+            type="symbol"
+            id="marker"
+            layout={{ 'icon-image': 'marker-15' }}>
+            <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+          </Layer>
+          <Cluster ClusterMarkerFactory={clusterMarker}
+          >
+            {
+              this.state.users.map((user, key) =>
+                <Marker
+                  key={key}
+                  style={styles.marker}
+                  coordinates={user.location}
+                  onClick={() => {
+                    this.changeCenter([20, 20]);
+                    this.changeZoom(10);
+                  }}
+                  // onClick={this.onMarkerClick.bind(this, feature.geometry.coordinates)}
+                  >
+                  <CircleIcon />
+                </Marker>
+              )
+            }
+          </Cluster>
+      </Map>
     );
   }
 }
 
-export default Map;
+export default MapComponent;
