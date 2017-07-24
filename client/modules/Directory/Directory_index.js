3baf0d6ddb7a7526bb4899354c7b989da09260dd
Alessandro Latif <alessandro@latif-khan.com>
2017-07-23 22:54:52 -0700

    changes to some bugs

--- client/modules/Directory/Directory_index.js
+++ client/modules/Directory/Directory_index.js
@@ -20,7 +20,7 @@ class Directory extends React.Component {
     return (
         <div>
           <h1 style={styles.header}>I am the directory</h1>
-          {/* <DeckContainer />*/}
+           {/* <DeckContainer /> */}
         </div>
     );
   }
