ba5b608271b2922e1852289f2d7760e1d922240f
omeshomes <omid.rooholfada@yale.edu>
2017-08-09 18:29:06 -0700

    search functionality for directory

--- client/modules/Directory2/Directory_index2.js
+++ client/modules/Directory2/Directory_index2.js
@@ -7,11 +7,15 @@ import { connect } from 'react-redux';
 import css from './Directory2.css';
 import getUsersThunk from '../../thunks/directory_thunks/getUsersThunk';
 import DirectoryCard from './Directory_Card';
+import Select from 'react-select';
+import uuidv4 from 'uuid/v4';
 
 class Directory2 extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      currentCards: [],
+      query: ''
     };
   }
 
@@ -25,12 +29,35 @@ class Directory2 extends React.Component {
     sessionStorage.setItem('url', urls);
   }
 
+  componentWillReceiveProps(nextProps) {
+    console.log('nextProps', nextProps);
+    this.setState({currentCards: nextProps.users});
+  }
+
+  handleChange(value) {
+    console.log('it works!', value);
+    const substring = (value ? value : '').toLowerCase();
+    const filteredCards = this.props.users.filter((user) => {return user.fullName.toLowerCase().includes(substring);});
+    this.setState({currentCards: filteredCards});
+  }
+
   render() {
-    console.log('here are the users', this.props.users);
+    console.log('here are the users', this.props.users, this.state.currentCards);
     return (
+      <div>
+        <Select
+          name="selected-state"
+          value={this.state.query}
+          simpleValue
+          autofocus
+          clearable
+          placeholder="Search by Name"
+          onInputChange={this.handleChange.bind(this)}
+        /> <br/>
         <div className="Page2">
-            {this.props.users.map(user =>
+            {this.state.currentCards.map(user =>
               <DirectoryCard
+              key={uuidv4()}
               picture={user.pictureURL}
               name={user.fullName}
               email={user.contact.email[0]}
@@ -39,6 +66,7 @@ class Directory2 extends React.Component {
               />
             )}
         </div>
+      </div>
     );
   }
 }
