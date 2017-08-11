aa8d01f140919bb54fbc4782504b9acc7949598a
Alessandro Latif <alessandro@latif-khan.com>
2017-08-11 16:20:34 -0700

    pushing to heroku

--- client/modules/App/App_Community.js
+++ client/modules/App/App_Community.js
@@ -7,7 +7,7 @@ import Discover from '../Discover/Discover_index';
 import Directory from '../Directory2/Directory_index2';
 import NavBar from './App_Community_NavBar';
 import MapContainer from '../Map/Map_index';
-import EditProfile from '../EditProfile/EditProfile_index';
+// import EditProfile from '../EditProfile/EditProfile_index';
 import updateLocationThunk from '../../thunks/map_thunks/updateLocationThunk';
 
 
